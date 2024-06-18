2068553e1462428e72e82800a7a8a984c0a6a3b3
53huzefa-zeenwala <huzefa.zeenwal@gmail.com>
2024-06-18 17:19:24 +0530

    redo

--- dist/index.js
+++ dist/index.js
@@ -70,20 +70,21 @@ app.post("/webhook", (req, res) => __awaiter(void 0, void 0, void 0, function* (
         console.log("data " + data);
     }
 }));
-// app.get("/webhook", (req, res) => {
-//   const mode = req.query["hub.mode"];
-//   const token = req.query["hub.verify_token"];
-//   const challenge = req.query["hub.challenge"];
-//   // check the mode and token sent are correct
-//   if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
-//     // respond with 200 OK and challenge token from the request
-//     res.status(200).send(challenge);
-//     console.log("Webhook verified successfully!");
-//   } else {
-//     // respond with '403 Forbidden' if verify tokens do not match
-//     res.sendStatus(403);
-//   }
-// });
+app.get("/webhook", (req, res) => {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+    // check the mode and token sent are correct
+    if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+        // respond with 200 OK and challenge token from the request
+        res.status(200).send(challenge);
+        console.log("Webhook verified successfully!");
+    }
+    else {
+        // respond with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);
+    }
+});
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 });
